f85870951cf64426e326f8494e9e1c0d412d2be8
Stuart Lee <stuart.lee@drpgroup.com>
2019-08-19 14:02:08 +0100

    basic audio

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 ### resources
 - https://lodev.org/cgtutor/raycasting.html
 - Modified tiles found here: http://finalbossblues.com/timefantasy/freebies/dark-dimension-tileset/
+- https://github.com/foumart/JS.13kGames/blob/master/lib/SoundFX.js
---
+++ src/js/audio.js
@@ -0,0 +1,68 @@
+const soundContext = new window.AudioContext()
+const oscTypes = ["square", "sawtooth", "triangle", "sine"]
+
+// start frequency HZ, frequency change, delay between changes, number of changes, volume, type
+function playSound (frequency, increment, delay, times, volume, type = 0) {
+
+  const oscillator = soundContext.createOscillator()
+  oscillator.frequency.value = frequency
+  oscillator.type = oscTypes[type]
+
+  // modulation for sound volume control
+  const modulationGain = soundContext.createGain();
+  modulationGain.gain.value = 0;
+
+  oscillator.connect(modulationGain);
+  modulationGain.connect(soundContext.destination);
+  oscillator.start();
+
+  let i = 0
+  const interval = setInterval(playTune, delay)
+
+  function playTune () {
+    oscillator.frequency.value = frequency + increment * i
+    modulationGain.gain.value = (1 - (i / times)) * volume
+    i++
+    if (i > times) {
+      clearInterval(interval)
+      setTimeout(stopTune, delay + times) // prevents the click when stopping the oscillator
+    }
+  }
+
+  function stopTune () {
+    oscillator.stop()
+  }
+}
+
+function soundjump () {
+  playSound(150, 30, 15, 20, 0.5)
+}
+function soundpew () {
+  playSound(920, -80, 20, 15, 0.5)
+}
+function soundzap () {
+  playSound(500, -200, 40, 10, 0.25, 1)
+}
+function soundbounce () {
+  playSound(260, -60, 15, 15, 0.5, 2)
+}
+function soundstuck () {
+  playSound(100, -10, 15, 15, 1, 2)
+}
+function soundexplosion () {
+  playSound(100, -10, 10, 25, 0.5)
+  playSound(125, -5, 20, 45, 0.1, 1)
+  playSound(40, 2, 20, 20, 1, 2)
+  playSound(200, -4, 10, 100, 0.25, 2)
+}
+function soundblow () {
+  playSound(120, -6, 20, 15, 0.1, 1)
+  playSound(40, -2, 20, 25, 1, 2)
+  playSound(60, 10, 15, 15, 0.1, 1)
+  playSound(160, -5, 20, 30, 0.1, 3)
+}
+function soundshot () {
+  playSound(160, 10, 15, 10, 0.1)
+  playSound(250, -20, 30, 10, 0.1, 1)
+  playSound(1500, -150, 30, 10, 0.1, 1)
+}
